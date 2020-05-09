e6c2b1ea4189cb6be4dc0adeb4c5c5397acc5e4a
ratna <ratnaanddev@gmail.com>
2020-05-09 19:44:02 +0530

    Final Commit

--- app/src/main/java/com/ratna/appyhightask/activities/HomeActivity.java
+++ app/src/main/java/com/ratna/appyhightask/activities/HomeActivity.java
@@ -93,12 +93,15 @@ public class HomeActivity extends AppCompatActivity implements IParseListener, V
         // Initialize the Google Mobile Ads SDK
         MobileAds.initialize(this,
                 getString(R.string.ad_unit_id));
+     /*   admob();
+        addNativeExpressAds();
+        setUpAndLoadNativeExpressAds();*/
 
         pushNotifications();
 
 
         loadNativeAds();
-
+        // loadMenu();
     }
 
 
@@ -113,10 +116,10 @@ public class HomeActivity extends AppCompatActivity implements IParseListener, V
 
 
     private void pushNotifications() {
+
         OneSignal.startInit(this)
                 .setNotificationReceivedHandler(new ExampleNotificationReceivedHandler())
                 .setNotificationOpenedHandler(new ExampleNotificationOpenedHandler())
-                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
                 .unsubscribeWhenNotificationsAreDisabled(true)
                 .init();
     }
@@ -237,6 +240,7 @@ public class HomeActivity extends AppCompatActivity implements IParseListener, V
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.india:
+                loadNativeAds();
                 code = "in";
                 if (Utility.isNetworkAvailable(this)) {
                     callServiceforNews();
@@ -246,6 +250,7 @@ public class HomeActivity extends AppCompatActivity implements IParseListener, V
                 }
                 return true;
             case R.id.usa:
+                loadNativeAds();
                 code = "us";
                 if (Utility.isNetworkAvailable(this)) {
                     callServiceforNews();
@@ -313,7 +318,24 @@ public class HomeActivity extends AppCompatActivity implements IParseListener, V
         @Override
         public void notificationReceived(OSNotification notification) {
             JSONObject data = notification.payload.additionalData;
+            String notificationID = notification.payload.notificationID;
+            String title = notification.payload.title;
+            String body = notification.payload.body;
+            String smallIcon = notification.payload.smallIcon;
+            String largeIcon = notification.payload.largeIcon;
+            String bigPicture = notification.payload.bigPicture;
+            String smallIconAccentColor = notification.payload.smallIconAccentColor;
+            String sound = notification.payload.sound;
+            String ledColor = notification.payload.ledColor;
+            int lockScreenVisibility = notification.payload.lockScreenVisibility;
+            String groupKey = notification.payload.groupKey;
+            String groupMessage = notification.payload.groupMessage;
+            String fromProjectNumber = notification.payload.fromProjectNumber;
+            String rawPayload = notification.payload.rawPayload;
+
             String customKey;
+
+            Log.i("OneSignalExample", "NotificationID received: " + notificationID);
 
             if (data != null) {
                 customKey = data.optString("customkey", null);
@@ -329,35 +351,65 @@ public class HomeActivity extends AppCompatActivity implements IParseListener, V
         // This fires when a notification is opened by tapping on it.
         @Override
         public void notificationOpened(OSNotificationOpenResult result) {
-            Log.i("OSNotificationPayload", "result.notification.payload.toJSONObject().toString(): " + result.notification.payload.toJSONObject().toString());
-            // Capture Launch URL (App URL) here
-
-
+            OSNotificationAction.ActionType actionType = result.action.type;
             JSONObject data = result.notification.payload.additionalData;
-            String customKey;
-            Log.e("data",""+data);
-            if (data != null) {
-                try {
-                    customKey = data.getString("customerkey");
-                    Log.e("key",""+customKey);
-                    if (customKey != null) {
-                        Intent intent = new Intent(HomeActivity.this, NewsDetailsActivity.class);
-                        // intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        intent.putExtra("newsurl", result.notification.payload.launchURL);
-                        Log.i("OneSignalExample", "openURL = " + result.notification.payload.launchURL);
-                        // startActivity(intent);
-                        startActivity(intent);
-                        Log.i("customkey", "customkey set with value: " + customKey);
-                    }else {
-                        Intent home=new Intent(HomeActivity.this,HomeActivity.class);
-                        startActivity(home);
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+            String launchUrl = result.notification.payload.launchURL; // update docs launchUrl
 
+            String customKey;
+            String openURL = launchUrl;
+           // Object activityToLaunch = HomeActivity.class;
+            if (openURL != null) {
+
+                Intent intent = new Intent(getApplicationContext(), NewsDetailsActivity.class);
+                // intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("newsurl", openURL);
+                Log.i("OneSignalExample", "openURL = " + openURL);
+                // startActivity(intent);
+                startActivity(intent);
+            }else {
+                Intent home=new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(home);
             }
+
+            if (data != null) {
+                customKey = data.optString("customkey", null);
+                openURL = data.optString("openURL", null);
+
+                if (customKey != null)
+                    Log.i("OneSignalExample", "customkey set with value: " + customKey);
+
+                if (openURL != null)
+                    Log.i("OneSignalExample", "openURL to webview with URL value: " + openURL);
+            }
+
+          /*  if (actionType == OSNotificationAction.ActionType.ActionTaken) {
+                Log.i("OneSignalExample", "Button pressed with id: " + result.action.actionID);
+
+                if (result.action.actionID.equals("id1")) {
+                    Log.i("OneSignalExample", "button id called: " + result.action.actionID);
+                    activityToLaunch = Splashscreen.class;
+                } else
+                    Log.i("OneSignalExample", "button id called: " + result.action.actionID);
+            }
+            // The following can be used to open an Activity of your choice.
+            // Replace - getApplicationContext() - with any Android Context.
+            // Intent intent = new Intent(getApplicationContext(), YourActivity.class);
+            Intent intent = new Intent(getApplicationContext(), (Class<?>) activityToLaunch);
+            // intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra("openURL", openURL);
+            Log.i("OneSignalExample", "openURL = " + openURL);
+            // startActivity(intent);
+            startActivity(intent);
+*/
+            // Add the following to your AndroidManifest.xml to prevent the launching of your main Activity
+            //   if you are calling startActivity above.
+        /*
+           <application ...>
+             <meta-data android:name="com.onesignal.NotificationOpened.DEFAULT" android:value="DISABLE" />
+           </application>
+        */
         }
     }
 }
